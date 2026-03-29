f903351c7581b3da091de5a0dc59b68521b39d10
Michał Pilecki <michal@MacBook-Micha.local>
2026-03-29 09:26:12 +0200

    fix nextjs pages for vercel

--- app/app/deals/[id]/page.tsx
+++ app/app/deals/[id]/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 export const dynamic = 'force-dynamic'
 import Link from "next/link";
 import { initialDeals, COLUMNS, formatCurrency } from "@/lib/mockData";
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 export const dynamic = 'force-dynamic'
 import KanbanBoard from "@/components/app/KanbanBoard";
 import type { Metadata } from "next";
